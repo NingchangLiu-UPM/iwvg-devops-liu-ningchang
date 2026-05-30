57f9d1b25421ea581539d2c634bcc032144b1ac9
NINGCHANGLIU <francisco0615lnc@gmail.com>
2026-05-30 19:48:40 +0200

    feat: implement fraction subtraction search #15

---
+++ src/main/java/es/upm/miw/devops/Searches.java
@@ -0,0 +1,43 @@
+package es.upm.miw.devops;
+
+import java.util.Optional;
+
+public class Searches {
+
+    private final UsersDatabase usersDatabase;
+
+    public Searches() {
+        this.usersDatabase = new UsersDatabase();
+    }
+
+    Searches(UsersDatabase usersDatabase) {
+        this.usersDatabase = usersDatabase;
+    }
+
+    public Fraction findFractionSubtractionByUserName(String name) {
+        Optional<User> user = usersDatabase.findAll()
+                .filter(u -> u.getName().equals(name))
+                .findFirst();
+        if (user.isEmpty() || user.get().getFractions().isEmpty()) {
+            return null;
+        }
+        return user.get().getFractions().stream()
+                .reduce(subtract())
+                .orElse(null);
+    }
+
+    private java.util.function.BinaryOperator<Fraction> subtract() {
+        return (a, b) -> subtract(a, b);
+    }
+
+    private Fraction subtract(Fraction minuend, Fraction subtrahend) {
+        int resultNumerator = minuend.getNumerator() * subtrahend.getDenominator()
+                - subtrahend.getNumerator() * minuend.getDenominator();
+        int resultDenominator = minuend.getDenominator() * subtrahend.getDenominator();
+        if (resultDenominator < 0) {
+            resultNumerator = -resultNumerator;
+            resultDenominator = -resultDenominator;
+        }
+        return new Fraction(resultNumerator, resultDenominator);
+    }
+}
